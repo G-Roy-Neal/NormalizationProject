0e93c20ae821f22ccaefc9a08481284dc291fa24
Dawson Vaal <dcvaal@bsu.edu>
2022-02-04 15:00:40 -0500

    Add nomalize_data function that normalizes the columns of data.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ with open('auto-mpg.csv', mode='r') as read_obj:
     reader = csv.reader(read_obj)
     dataAsRows = list(reader)
 
-
 def mainMenu():
     print(dataAsRows[askUserInput()])
     print(dataAsRows[askUserInput()])
@@ -21,7 +20,27 @@ def askUserInput():
             print("That was an invalid number, please re-enter (1-90): ")
             Index = int(input("Enter number : "))
 
+def normalize_data(data, min, max):
+    normal_column = []
+    for i in range(len(data)):
+        new_value = round((data[i] - min) / (max - min), 2)
+        normal_column.append(new_value)
+    return normal_column
+
+
+
 
 if __name__ == '__main__':
     open('auto-mpg.csv')
+
+    normalized_data = []
+    for j in range(0,4):
+        columnList = []
+        for i in range(1, len(dataAsRows)):
+                columnList.append(int(dataAsRows[i][j]))
+        normalized_data.append(normalize_data(columnList, min(columnList), max(columnList)))
+
+    print(normalized_data)
+
     mainMenu()
+
